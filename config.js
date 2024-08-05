1714ef4a3972301505b18f5167638f509a77f3d7
Lil Presh 🎗🎖 <solomonoluwatobi307@gmail.com>
2024-08-05 01:22:49 +0100

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const DATABASE_URL =
   process.env.DATABASE_URL === undefined ? databasePath : process.env.DATABASE_URL
 module.exports = {
   VERSION: require('./package.json').version,
-  SESSION_ID: (process.env.SESSION_ID || '').trim(),
+  SESSION_ID: (process.env.SESSION_ID || '5_8_7f76_29fd_494a').trim(),
   DATABASE:
     DATABASE_URL === databasePath
       ? new Sequelize({
